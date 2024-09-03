71d6cd66b936e3e43e4382fc209af149b707179b
PORTAFOLIO <bragon14@hotmail.com>
2024-09-03 04:59:34 -0600

    ADD-MANUAL-INSTRUCTIONS
    NEW INSTRUCTION USER

--- javascript.js
+++ javascript.js
@@ -1,3 +1,63 @@
+        //Oculto todos los textos excepto el texto inicial
+        document.getElementById("texto1").style.display = "none";
+        document.getElementById("texto2").style.display = "none";
+        document.getElementById("texto3").style.display = "none";
+        document.getElementById("texto4").style.display = "none";
+
+        function seleccion() {
+
+            if (document.getElementById("blanco").selected == true) {
+                document.getElementById("texto1").style.display = "none";
+                document.getElementById("texto2").style.display = "none";
+                document.getElementById("texto3").style.display = "none";
+                document.getElementById("texto4").style.display = "none";
+                document.getElementById("blanco1").style.display = "block";
+
+            }
+
+            if (document.getElementById("opcion1").selected == true) {
+
+                document.getElementById("texto2").style.display = "none";
+                document.getElementById("texto3").style.display = "none";
+                document.getElementById("texto4").style.display = "none";
+                document.getElementById("blanco1").style.display = "none";
+                document.getElementById("texto1").style.display = "block";
+
+                console.log("Has seleccionado Opcion 1");
+
+            } else if (document.getElementById("opcion2").selected == true) {
+
+                document.getElementById("blanco1").style.display = "none";
+                document.getElementById("texto1").style.display = "none";
+                document.getElementById("texto4").style.display = "none";
+                document.getElementById("texto3").style.display = "none";
+                document.getElementById("texto2").style.display = "block";
+                console.log("Has seleccionado Opcion 2");
+
+            } else if (document.getElementById("opcion3").selected == true) {
+
+                document.getElementById("blanco1").style.display = "none";
+                document.getElementById("texto1").style.display = "none";
+                document.getElementById("texto2").style.display = "none";
+                document.getElementById("texto3").style.display = "block";
+                document.getElementById("texto4").style.display = "none";
+                console.log("Has seleccionado Opción 3");
+
+            } else if (document.getElementById("opcion4").selected == true) {
+
+                document.getElementById("blanco1").style.display = "none";
+                document.getElementById("texto1").style.display = "none";
+                document.getElementById("texto2").style.display = "none";
+                document.getElementById("texto3").style.display = "none";
+                document.getElementById("texto4").style.display = "block";
+                console.log("Has seleccionado Opción 4");
+
+            } 
+        }
+
+
+
+
 $(document).ready(function(){
 
 $('#Select').on('change', function(){
@@ -7,12 +67,10 @@ $('#fila1').children('div').hide();
 $('#fila1').children(SelectValor).show();
 
 
-  
+});
 });
 
-});
-
-
+ /*ESTRUCTURA*/
 
 $(document).ready(function(){
     $("#EXTREMO-INFERIOR").click(function(){
@@ -38,8 +96,9 @@ $(document).ready(function(){
     });
 });
 
+ /*FIN ESTRUCTURA*/
 
-<!--INTERNO-->
+/*INTERNO*/
 
 $(document).ready(function(){
     $("#INTERNO-IZQUIERDO").click(function(){
@@ -47,14 +106,15 @@ $(document).ready(function(){
     });
 });
 
-
 $(document).ready(function(){
     $("#INTERNO-DERECHO").click(function(){
         $("#INTERNO-DERECHOMODAL").modal();
     });
 });
 
-<!--COLGAROPA-->
+/*FIN INTERNO*/
+
+/*COLGAROPA*/
 
 $(document).ready(function(){
     $("#COLGAROPA").click(function(){
@@ -62,7 +122,9 @@ $(document).ready(function(){
     });
 });
 
-<!--SQUADS-->
+/*FIN COLGAROPA*/
+
+/*SQUADS*/
 
 $(document).ready(function(){
     $("#SQUADS").click(function(){
@@ -70,8 +132,9 @@ $(document).ready(function(){
     });
 });
 
-<!--RIGHT SECTION-->
+/*FIN SQUADS*/
 
+/*RIGHT SECTION*/
 $(document).ready(function(){
     $("#DERECHO-DIVISION1").click(function(){
         $("#DERECHO-DIVISIONMODAL").modal();
@@ -84,7 +147,9 @@ $(document).ready(function(){
     });
 });
 
-<!--CENTER SECTION-->
+/*FIN RIGHT SECTION*/
+
+/*CENTER SECTION*/
 
 $(document).ready(function(){
     $("#CENTRAL-DIVISION1").click(function(){
@@ -103,7 +168,10 @@ $(document).ready(function(){
         $("#CENTRAL-DIVISIONMODAL").modal();
     });
 });
-<!--LEFT SECTION-->
+
+/*FIN CENTER SECTION*/
+
+/*LEFT SECTION*/
 
 $(document).ready(function(){
     $("#IZQUIERDO-DIVISION1").click(function(){
@@ -117,12 +185,15 @@ $(document).ready(function(){
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#IZQUIERDO-DIVISION2").click(function(){
         $("#IZQUIERDO-DIVISIONMODAL").modal();
     });
 });
+
+/*FIN LEFT SECTION*/
+
+/*TORNILLOS*/
 
 <!--LEFT SECTION-->
 $(document).ready(function(){
@@ -152,7 +223,9 @@ $(document).ready(function(){
     });
 });
 
-<!--LEFT SECTION---------------------------------------------------->
+/*FIN-TORNILLOS*/
+
+/*PUERTAS*/
 $(document).ready(function(){
     $("#PUERTA-EXTREMO-IZQUIERDA1").click(function(){
         $("#PUERTA-EXTREMO-IZQUIERDA-MODAL").modal();
@@ -184,10 +257,9 @@ $(document).ready(function(){
     });
 });
 
+/*FIN PUERTAS*/
 
-<!--Contenedor-->
-
-<!--LEFT SECTION-->
+/*--CERRADURA - CAJONES*/
 $(document).ready(function(){
     $("#LOCK").click(function(){
         $("#CERRADURA1").modal();
@@ -201,106 +273,78 @@ $(document).ready(function(){
     });
 });
 
-<!--Contenedor-->
+/*--FIN CERRADURA - CAJONES*/
 
-<!--LEFT SECTION-->
+/*SLIDER*/
+
 $(document).ready(function(){
     $("#SLIDER").click(function(){
         $("#SLID").modal();
     });
 });
 
-<!--LEFT SECTION-->
+/*FIN SLIDER*/
+
+/*MANIJAS*/
+
 $(document).ready(function(){
     $("#MANIJA").click(function(){
         $("#MANIJAMODAL").modal();
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#MANIJA-CAJON").click(function(){
         $("#MANIJA-CAJONMODAL").modal();
     });
 });
 
-<!--LEFT SECTION-->
+/*FIN MANIJAS*/
+
+/*TRIPLEX*/
+
 $(document).ready(function(){
     $("#TRIPLEX-IZQUIERDO").click(function(){
         $("#TRIPLEX-IZQUIERDOMODAL").modal();
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#TRIPLEX-CENTRAL").click(function(){
         $("#TRIPLEX-CENTRALMODAL").modal();
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#TRIPLEX-DERECHO").click(function(){
         $("#TRIPLEX-IZQUIERDOMODAL").modal();
     });
 });
 
-<!--LEFT SECTION-->
+/*FIN TRIPLEX*/
+
+/*ESPECIFICACION TRIPLEX*/
 $(document).ready(function(){
     $("#CIRCULO1").click(function(){
         $("#CIRCULO").modal();
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#CIRCULO2").click(function(){
         $("#CIRCULO").modal();
     });
 });
 
-<!--LEFT SECTION-->
 $(document).ready(function(){
     $("#CIRCULO3").click(function(){
         $("#CIRCULO").modal();
     });
 });
 
+/*FIN ESPECIFICACION TRIPLEX*/
 
-/*
-
-<!--SELECTOR-->
-function listenToClicks(){
-    const buttons = document.querySelectorAll(".clickable-button");
-
-
-
-    buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        activateBoxes(button.id);
-    });
-   });
-}
-
-   function activateBoxes(buttonId){
-    const boxes = document.querySelectorAll(".box");
-
-    boxes.forEach((box) => {
-        if (box.classList.contains(buttonId)) {
-            box.dataset.active = true;
-        }else{
-            box.dataset.active = false;
-        }
-    });
-   }
-
-   listenToClicks();
-<!--SELECTOR-->
-
- 
-*/
-
-/*<!--SELECTOR1-->*/
+/*SELECTOR1*/
 function listenToClicks(){
     const botones = document.querySelectorAll(".clickable-g");
 
@@ -326,35 +370,8 @@ function listenToClicks(){
    }
 
  listenToClicks();
- /*<!--SELECTOR1-->*/
+ /*FIN SELECTOR1*/
 
- /*<!--SELECTOR2-->
-function listenToClicks(){
-    const botones2 = document.querySelectorAll(".clickable-g2");
-
-
-
-    botones2.forEach((g) => {
-    g.addEventListener("click", () => {
-        activateCajas(g.id);
-    });
-   });
-}
-
-   function activateCajas(gId){
-    const cajas = document.querySelectorAll(".caja");
-
-    cajas.forEach((caja) => {
-        if (caja.classList.contains(gId)) {
-            caja.dataset.active = true;
-        }else{
-            caja.dataset.active = false;
-        }
-    });
-   }
-
- listenToClicks();
- /*<!--SELECTOR2-->*/
 
 
 const btnDepartamentos = document.getElementById('btn-departamentos'),
@@ -515,10 +532,460 @@ m13.src = "SOUND/PIEZAS/center-door.mp3";
 
 /* PIEZAS*/
 
-
-
-
-
-
 /* INCLUSION DE SONIDO*/
+
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+/* MANUAL-INSTRUCCIONES-MUEBLE-COCINA*/
+
+/*INICIO-PIEZAS*/
+
+/*MODALES*/
+
+/*MODALES-MADERA-PIEZAS*/
+
+$(document).ready(function(){
+    $("#DIVISION-VERTICAL-INTERNA").click(function(){
+        $("#DIVISION-VERTICAL-INTERNAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#LATERALES").click(function(){
+        $("#LATERALESMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#BASE-TECHO").click(function(){
+        $("#BASE-TECHOMODAL").modal();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#TABLITA").click(function(){
+        $("#TABLITAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-MUEBLE").click(function(){
+        $("#SOPORTE-MUEBLEMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-PARED5").click(function(){
+        $("#SOPORTE-PARED5MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNAMODAL").modal();
+    });
+});
+$(document).ready(function(){
+    $("#TRIPLEX").click(function(){
+        $("#TRIPLEXMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PUERTAS-2").click(function(){
+        $("#PUERTAS-2MODAL").modal();
+    });
+});
+
+/*FIN-MADERA-PIEZAS*/
+
+/*ACCESORIOS-MUEBLE*/
+
+$(document).ready(function(){
+    $("#PUNTILLAS").click(function(){
+        $("#PUNTILLASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#MANIJAS").click(function(){
+        $("#MANIJASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA").click(function(){
+        $("#TORNILLERIAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#BISAGRAS").click(function(){
+        $("#BISAGRASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTES-2").click(function(){
+        $("#SOPORTES-2MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA-SOPORTE-4").click(function(){
+        $("#TORNILLERIA-SOPORTE-4MODAL").modal();
+    });
+});
+
+/*FIN ACCESORIOS-MUEBLE*/
+
+/*ESPECIFICACIONES-MUEBLE*/
+
+$(document).ready(function(){
+    $("#CORTE-SOPORTE-DETALLADO-PARED").click(function(){
+        $("#CORTE-SOPORTE-DETALLADO-PAREDMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#CORTE-SOPORTE-DETALLADO-MUEBLE").click(function(){
+        $("#CORTE-SOPORTE-DETALLADO-MUEBLEMODAL").modal();
+    });
+});
+
+/*FIN ESPECIFICACIONES-MUEBLE*/
+
+/*FIN MODALES*/
+
+/*MODALES-MADERA-PIEZAS*/
+
+$(document).ready(function(){
+    $("#DIVISION-VERTICAL-INTERNA1").click(function(){
+        $("#DIVISION-VERTICAL-INTERNAMODAL1").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-VERTICAL-INTERNA2").click(function(){
+        $("#DIVISION-VERTICAL-INTERNA2MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#LATERALES1").click(function(){
+        $("#LATERALESMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#BASE-TECHO1").click(function(){
+        $("#BASE-TECHOMODAL").modal();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#TABLITA1").click(function(){
+        $("#TABLITAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-MUEBLE1").click(function(){
+        $("#SOPORTE-MUEBLEMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-PARED1").click(function(){
+        $("#SOPORTE-PAREDMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA1").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNAMODAL").modal();
+    });
+});
+$(document).ready(function(){
+    $("#TRIPLEX1").click(function(){
+        $("#TRIPLEXMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA1").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PUERTAS-21").click(function(){
+        $("#PUERTAS-2MODAL").modal();
+    });
+});
+
+/*FIN-MADERA-PIEZAS*/
+
+/*ACCESORIOS-MUEBLE*/
+
+$(document).ready(function(){
+    $("#PUNTILLAS1").click(function(){
+        $("#PUNTILLASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#MANIJAS1").click(function(){
+        $("#MANIJASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA1").click(function(){
+        $("#TORNILLERIAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#BISAGRAS1").click(function(){
+        $("#BISAGRASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTES-21").click(function(){
+        $("#SOPORTES-2MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA-SOPORTE-41").click(function(){
+        $("#TORNILLERIA-SOPORTE-4MODAL").modal();
+    });
+});
+
+/*FIN ACCESORIOS-MUEBLE*/
+
+/*ESPECIFICACIONES-MUEBLE*/
+
+$(document).ready(function(){
+    $("#CORTE-SOPORTE-DETALLADO-PARED1").click(function(){
+        $("#CORTE-SOPORTE-DETALLADO-PAREDMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#CORTE-SOPORTE-DETALLADO-MUEBLE1").click(function(){
+        $("#CORTE-SOPORTE-DETALLADO-MUEBLEMODAL").modal();
+    });
+});
+
+/*FIN ESPECIFICACIONES-MUEBLE*/
+
+/*FIN-PIEZAS*/
+
+
+
+/*ACCESORIOS-MUEBLE*/
+
+$(document).ready(function(){
+    $("#LATERAL-DERECHA").click(function(){
+        $("#LATERAL-DERECHAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#LATERAL-IZQUIERDA").click(function(){
+        $("#LATERAL-IZQUIERDAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#BASE").click(function(){
+        $("#BASEAMODAL").modal();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#TECHO").click(function(){
+        $("#TECHOMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA1").click(function(){
+        $("#TORNILLERIAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-EXTREMA-IZQUIERDA").click(function(){
+        $("#DIVISION-HORIZONTAL-EXTREMA-IZQUIERDAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA-DERECHA").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNA-DERECHAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-EXTREMA-DERECHA").click(function(){
+        $("#DIVISION-HORIZONTAL-EXTREMA-DERECHAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#DIVISION-HORIZONTAL-INTERNA-IZQUIERDA").click(function(){
+        $("#DIVISION-HORIZONTAL-INTERNA-IZQUIERDAMODAL").modal();
+    });
+});
+
+/*FIN ACCESORIOS-MUEBLE*/
+
+/*ESPECIFICACIONES-MUEBLE*/
+
+$(document).ready(function(){
+    $("#DIVISIONES-VERTICALES").click(function(){
+        $("#DIVISIONES-VERTICALESMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA-SOPORTES").click(function(){
+        $("#TORNILLERIA-SOPORTESMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTES3").click(function(){
+        $("#SOPORTES3MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TABLITA-DELANTERA").click(function(){
+        $("#TABLITA-DELANTERAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TABLITA-TRASERAS").click(function(){
+        $("#TABLITA-TRASERASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TABLITA-TRASERA-ESCONDIDA").click(function(){
+        $("#TABLITA-TRASERA-ESCONDIDAMODAL").modal();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#TORNILLERIA-EXTERIOR").click(function(){
+        $("#TORNILLERIA-EXTERIORMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA-TABLITAS-TRASERAS").click(function(){
+        $("#TORNILLERIA-TABLITAS-TRASERASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TORNILLERIA-TABLITA-DELANTERA").click(function(){
+        $("#TORNILLERIA-TABLITA-DELANTERAMODAL").modal();
+    });
+});
+/*FIN ESPECIFICACIONES-MUEBLE*/
+
+/*ESPECIFICACIONES-PUERTAS*/
+$(document).ready(function(){
+    $("#TABLITA-PUERTA").click(function(){
+        $("#TABLITA-PUERTAMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PUERTAS-PUERTAS").click(function(){
+        $("#PUERTAS-PUERTASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#MANIJAS-PUERTAS").click(function(){
+        $("#MANIJAS-PUERTASMODAL").modal();
+    });
+});
+
+/*FIN ESPECIFICACIONES-PUERTAS*/
+
+/*FIN-PIEZAS*/
+
+
+/*ESPECIFICACIONES-PUERTAS*/
+$(document).ready(function(){
+    $("#TABLITAS-TRASERAS").click(function(){
+        $("#TABLITAS-TRASERASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PUNTILLAS5").click(function(){
+        $("#PUNTILLAS5MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-MUEBL2").click(function(){
+        $("#SOPORTE-MUEBLE2MODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#SOPORTE-PARED").click(function(){
+        $("#SOPORTE-PAREDMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PROCESO-SOPORTE-MUEBLE").click(function(){
+        $("#PROCESO-SOPORTE-MUEBLEMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PROCESO-PUNTILLAS").click(function(){
+        $("#PROCESO-PUNTILLASMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#PROCESO-SOPORTE-PARED").click(function(){
+        $("#PROCESO-SOPORTE-PAREDMODAL").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#TRIPLEX5").click(function(){
+        $("#TRIPLEX5MODAL").modal();
+    });
+});
+/*FIN ESPECIFICACIONES-PUERTAS*/
+
+/*FIN-PIEZAS*/
 
